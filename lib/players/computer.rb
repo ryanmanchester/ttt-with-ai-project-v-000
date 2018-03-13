459b318b05bf11d27fc6620841ddb959880c545c
Ryan Manchester <ryanm17@gmail.com>
2018-03-13 21:00:28 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,7 +5,8 @@ module Players
 
 
     def move(board)
-      binding.pry
+      valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      board.valid_move?("5") ? "5" : other_moves(board)
 
     end
 
