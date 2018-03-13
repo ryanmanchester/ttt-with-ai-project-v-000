68b556cebf8267347740fadb258776597e995000
Ryan Manchester <ryanm17@gmail.com>
2018-03-13 20:57:38 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,15 +3,9 @@ require 'pry'
 module Players
   class Computer < Player
 
-#valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
     def move(board)
-#binding.pry
-
-         if !board.taken?("5")
-          "5"
-
-      end
-    end
+      binding.pry
 
   def corner(board)
     corners = [1, 5, 7, 9]
